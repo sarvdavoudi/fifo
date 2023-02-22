f40fdbfb0b00e8e087e0e11afd8173eb1275f72d
sarvdavoudi <sarvdavoudi3000@yahoo.com>
2023-02-23 00:20:08 +0330

    new version

--- version 1-with array methods/fifo.js
+++ version 1-with array methods/fifo.js
@@ -1,21 +1,19 @@
-let unshiftArray = [1, 2, 3, 4, 5];
-let popAray = [1, 2, 3, 4, 5];
-let variable;
+let mainArrayForPop = [1, 2, 3, 4, 5];
+let mainArrayForUnshift = [1, 2, 3, 4, 5];
 
 //به اول آرایه اضافه میکند
 function unshift(variable) {
-  let temp = unshiftArray.unshift(variable);
+  let temp = mainArrayForUnshift.unshift(variable);
   return temp;
 }
 
 //از آخر آرایه حذف میکند
 function pop() {
-  let temp = popAray.pop();
+  let temp = mainArrayForPop.pop();
   return temp;
 }
+let showAfterUnshift = unshift(0);
+let showAfterpop = pop();
 
-unshift(0);
-pop();
-
-console.log(unshiftArray);
-console.log(popAray);
+console.log(`array after pop is: ${mainArrayForPop} and deleted item is :${showAfterpop}`);
+console.log(`array after unshift is ${mainArrayForUnshift}`);
